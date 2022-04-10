87c6664e707a1c640e70ddd9873ccf5726033d37
chidi-godwin <chidieberen1998@gmail.com>
2022-04-10 13:33:59 +0100

    update host

--- fyp/settings.py
+++ fyp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['https://sees-fyp.herokuapp.com/']
 
 
 # Application definition
