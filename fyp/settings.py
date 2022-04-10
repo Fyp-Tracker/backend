5fb4636955b7901465e3e41ed0534f3523bb40dd
chidi-godwin <chidieberen1998@gmail.com>
2022-04-10 15:07:28 +0100

    set blacklist after rotation

--- fyp/settings.py
+++ fyp/settings.py
@@ -137,8 +137,9 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
     'UPDATE_LAST_LOGIN': True,
+    'BLACKLIST_AFTER_ROTATION': False
 }
 
 DJOSER = {
